4440c287e7feec37a2a93967a9a6eafb9f3dbe63
Liam Orr <orrbit76@gmail.com>
2022-06-28 08:14:00 -0400

    Add project files.

---
+++ ProjectEuler/Problems/EvenFibonacciNumbers/EvenFibonacciNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems.EvenFibonacciNumbers
+{
+    public class EvenFibonacciNumbers : ISolvable
+    {
+        public int Solve()
+        {
+            var prev = 1;
+            var curr = 2;
+
+            var limit = 4000000;
+
+            var sum = curr;
+
+            while (curr < limit)
+            {
+                var temp = curr + prev;
+                if (temp % 2 == 0)
+                    sum += temp;
+
+                prev = curr;
+                curr = temp;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,15 @@
+﻿using ProjectEuler.Problems.EvenFibonacciNumbers;
+using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new EvenFibonacciNumbers();
+
+            Console.WriteLine($"Answer: {solution}");
+        }
+    }
+}
