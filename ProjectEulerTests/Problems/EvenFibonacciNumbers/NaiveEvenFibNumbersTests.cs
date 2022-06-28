ecf3bd48ebfe9007e3928c1694b277a5d68807c8
Liam Orr <orrbit76@gmail.com>
2022-06-28 10:28:48 -0400

    Develop tests for the even fib numbers

---
+++ ProjectEulerTests/Problems/EvenFibonacciNumbers/NaiveEvenFibNumbersTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Problems.EvenFibonacciNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems.EvenFibonacciNumbers.Tests
+{
+    [TestClass()]
+    public class NaiveEvenFibNumbersTests
+    {
+        [TestMethod()]
+        public void SolveTest()
+        {
+            var solver = new NaiveEvenFibNumbers();
+            Assert.AreEqual(4613732, solver.Solve());
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems/EvenFibonacciNumbers/ReducedIterationsEvenFibNumbersTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Problems.EvenFibonacciNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems.EvenFibonacciNumbers.Tests
+{
+    [TestClass()]
+    public class ReducedIterationsEvenFibNumbersTests
+    {
+        [TestMethod()]
+        public void SolveTest()
+        {
+            var solver = new ReducedIterationsEvenFibNumbers();
+            Assert.AreEqual(4613732, solver.Solve());
+        }
+    }
+}
