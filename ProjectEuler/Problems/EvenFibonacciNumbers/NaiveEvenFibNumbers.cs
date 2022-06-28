e5235d37306e9e0505137ca4a6c3308d8ec1aa2e
Liam Orr <orrbit76@gmail.com>
2022-06-28 10:30:53 -0400

    Improve on the naive solution by eliminating 2/3 conditional checks for even numbers

--- ProjectEuler/Problems/EvenFibonacciNumbers/NaiveEvenFibNumbers.cs
+++ ProjectEuler/Problems/EvenFibonacciNumbers/NaiveEvenFibNumbers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler.Problems.EvenFibonacciNumbers
 {
-    public class EvenFibonacciNumbers : ISolvable
+    public class NaiveEvenFibNumbers : ISolvable
     {
+
+        [Benchmark]
         public int Solve()
         {
             var prev = 1;
---
+++ ProjectEuler/Problems/EvenFibonacciNumbers/ReducedIterationsEvenFibNumbers.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems.EvenFibonacciNumbers
+{
+    public class ReducedIterationsEvenFibNumbers: ISolvable
+    {
+        /// <summary>
+        /// This method makes use of the mathematical observation
+        /// that even fibonacci numbers occur on a predictable basis.
+        /// This is because of the fact that
+        /// even + even = even
+        /// odd + even = odd
+        /// odd + odd = even
+        /// 
+        /// Look at the first couple numbers in the sequence
+        /// 1, 2, 3, 5, 8, 13, 21, 34
+        /// odd, even, odd, odd, even, odd, odd, even
+        /// 
+        /// We can observe the atomic pattern of odd, even, odd, which means
+        /// we can eliminate conditional checks for 2/3 of numbers
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Benchmark]
+        public int Solve()
+        {
+            var prev = 1;
+            var curr = 2;
+
+            var limit = 4000000;
+            var sum = curr;
+            while(true)
+            {
+                var odd1 = prev + curr;
+                var odd2 = odd1 + curr;
+
+                prev = odd2;
+                curr = odd1 + odd2;
+
+                if (curr > limit)   //This branching point doesn't waste a lot of efficiency since it is predicted by the processor to be false majority of loops
+                    break;
+                sum += curr;
+            }
+
+            return sum;
+        }
+    }
+}
