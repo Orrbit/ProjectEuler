82f0b65da0d35b1bc6026607a5f745c1f0b78cb0
Liam Orr <orrbit76@gmail.com>
2022-07-06 16:35:46 -0400

    Finish the sum square difference problem

---
+++ ProjectEuler/Problems/SumSquareDifference/NaiveSumSquareDifference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems.SumSquareDifference
+{
+    public class NaiveSumSquareDifference
+    {
+        public int Solve()
+        {
+            int sumSquares = 0;
+            int sum = 0;
+
+            for(int i = 1; i <= 100; i++)
+            {
+                sumSquares += (int)Math.Pow(i,2);
+                sum += i;
+            }
+            return (int)Math.Pow(sum, 2) - sumSquares;
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems/SumSquareDifference/NaiveSumSquareDifferenceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Problems.SumSquareDifference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems.SumSquareDifference.Tests
+{
+    [TestClass()]
+    public class NaiveSumSquareDifferenceTests
+    {
+        [TestMethod()]
+        public void SolveTest()
+        {
+            var solver = new NaiveSumSquareDifference();
+            Assert.AreEqual(25164150, solver.Solve());
+        }
+    }
+}
